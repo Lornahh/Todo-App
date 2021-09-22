448962a8cf1777284cadeacee78da219f979f370
unknown <quiksniping1@gmail.com>
2021-09-21 23:31:03 -0400

    Added basis of todo app - rendered a very ugly list of proper items

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TodoItem from "./components/TodoItem";
 
-function App() {
+const TodoList = () => {
+  const TodoListData = [
+    {
+      Name: "Dishes",
+      Completed: false,
+      Id: 0,
+    },
+    {
+      Name: "Trash",
+      Completed: true,
+      Id: 1,
+    },
+    {
+      Name: "Laundry",
+      Completed: false,
+      Id: 2,
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Welcome to the todo list!</h1>
+      <div className="TodoListContainer">
+        <ul>
+          {TodoListData.map((TodoListItem) => (
+            <TodoItem TodoListItem={TodoListItem} key={TodoListItem.Id} />
+          ))}
+          
+        </ul>
+        <h2>What the heck</h2>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default TodoList;
